8831c8440039804ba4097fe4a9acb8891ea0946d
Vyaliy <Vyaltsevn@mail.ru>
2023-04-14 17:22:25 +0300

    Добавьте файлы проекта.

---
+++ Module16_Practice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module16_Practice
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module16_Prcatice.Tests/CalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Practices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module16_Prcatice.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Additional(2, 3) == 5);
+        }
+        [Test]
+        public void Substration_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Subtraction(2, 3) == -1);
+        }
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Miltiplication(2, 3) == 6);
+        }
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.True(calculator.Division(6, 3) == 2);
+        }
+        [Test]
+        public void Division_MustThrowDivisionByZeroException()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(2, 0));
+        }
+    }
+}
